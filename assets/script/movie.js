074afbc91d9394e8977a3bd472c646f915393807
Drigo <rodrigoaguayo94@gmail.com>
2021-04-28 10:24:13 -0700

    updating moviepage.html and movie.js

--- assets/script/movie.js
+++ assets/script/movie.js
@@ -18,8 +18,11 @@ $(document).ready(function () {
     });
 });
 
+var genreTags = $('#show_genre');
+
 function randomMovie() {
 
+    // gets the stored values from the checkboxes
     var selected = localStorage.getItem('checked');
 
     var apiURL = `https://api.themoviedb.org/3/discover/movie?api_key=2d68f36569896b3eca3f4d442ec3c9a3&language=en-US&sort_by=popularity.desc&vote_count.gte=50&certification_country=US&include_adult=false&watch_region=US` + selected + '&page=`'
@@ -40,13 +43,16 @@ function randomMovie() {
             }).then(function (data) {
                 console.log(data.results[randomResult])
                 // appends the poster
-                $('#show_poster').append(`<div class="card" style="width: 18rem;">
-                <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${data.results[randomResult].poster_path}" class="card-img-top" alt="...">
-                <div class="card-body"> `);
+                $('#show_poster').append(`
+                <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${data.results[randomResult].poster_path}" class="card-img-top" alt="..." style="max-width: fit-content; max-height:512px; border-radius:10px; margin-top: 30px;">
+                 `);
                 // appends the movie title
-                $('#show_title').append(`${data.results[randomResult].original_title.toUpperCase()}`)
+                $('#show_title').append(`${data.results[randomResult].original_title.toUpperCase()}`);
                 // append movie desc.
-                $('#show_desc').append(`${data.results[randomResult].overview}`)
+                $('#show_desc').append(`${data.results[randomResult].overview}`);
+                $(document).ready(function () {
+                    $("#bg").css("background-image", `url(https://image.tmdb.org/t/p/w500/${data.results[randomResult].backdrop_path})`);
+                });
                 $("body").append(`
           <div class="card" style="width: 18rem;">
             <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${data.results[randomResult].poster_path}" class="card-img-top" alt="...">
@@ -63,7 +69,69 @@ function randomMovie() {
             </div>
           </div>`);
             })
+            var movieURL = `https://api.themoviedb.org/3/movie/${data.results[randomResult].id}?api_key=2d68f36569896b3eca3f4d442ec3c9a3&language=en-US`
+            fetch(movieURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    console.log(response);
+                    console.log(response.genres[0].name);
+                    // creating new array to set all tags in a single tag
+                    var genreTag = []
+                    for (var i = 0; i < response.genres.length; i++) {
+
+                        genreTag.push(response.genres[i].name);
+                    };
+
+                    var genreNames = genreTag.join(',');
+                    $('#show_genre').append(`<p>${genreNames}</p>`);
+                });
+            var movieURL = `https://api.themoviedb.org/3/movie/${data.results[randomResult].id}/credits?api_key=2d68f36569896b3eca3f4d442ec3c9a3&language=en-US`
+            fetch(movieURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    console.log(response);
+                    // appending first five actors from response
+                    $('#actor1').append(response.cast[0].name);
+                    $('#actor2').append(response.cast[1].name);
+                    $('#actor3').append(response.cast[2].name);
+                    $('#actor4').append(response.cast[3].name);
+                    $('#actor5').append(response.cast[4].name);
+                    // appending characters the actors are playing. 
+                    $('#character1').append(response.cast[0].character);
+                    $('#character2').append(response.cast[1].character);
+                    $('#character3').append(response.cast[2].character);
+                    $('#character4').append(response.cast[3].character);
+                    $('#character5').append(response.cast[4].character);
+                    // appending movie actor poster
+                    $('#img_actor1').append(`
+                        <img src="https://www.themoviedb.org/t/p/w138_and_h175_face/${response.cast[0].profile_path}" class="card-img-top" alt="..." style="max-width: fit-content; max-height:175px; border-radius:10px;">
+                         `);
+                    $('#img_actor2').append(`
+                         <img src="https://www.themoviedb.org/t/p/w138_and_h175_face/${response.cast[1].profile_path}" class="card-img-top" alt="..." style="max-width: fit-content; max-height:175px; border-radius:10px;">
+                          `);
+                    $('#img_actor3').append(`
+                        <img src="https://www.themoviedb.org/t/p/w138_and_h175_face/${response.cast[2].profile_path}" class="card-img-top" alt="..." style="max-width: fit-content; max-height:175px; border-radius:10px;">
+                         `);
+                    $('#img_actor4').append(`
+                         <img src="https://www.themoviedb.org/t/p/w138_and_h175_face/${response.cast[3].profile_path}" class="card-img-top" alt="..." style="max-width: fit-content; max-height:175px; border-radius:10px;">
+                          `);
+                    $('#img_actor5').append(`
+                          <img src="https://www.themoviedb.org/t/p/w138_and_h175_face/${response.cast[4].profile_path}" class="card-img-top" alt="..." style="max-width: fit-content; max-height:175px; border-radius:10px;">
+                           `);
+
+
+                    console.log(genreTags)
+
+                    console.log(Tag1);
+
+                });
         })
+
 };
+
 
 randomMovie();
